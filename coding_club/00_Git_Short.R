623a7d3b646d4ec8b474b074438af6ca31b1e68c
tk51 <t.khamardiuk@gmail.com>
2023-03-21 19:59:29 +0100

    file updates

---
+++ coding_club/00_Git_Short.R
@@ -0,0 +1,25 @@
+getwd()
+setwd("/home/t51/tmp")
+
+usethis::git_remotes()
+
+# or there is another option as:
+  usethis::use_git_remote(
+    "origin",
+    "https:://github.com/tk51/coding-demo.git",
+    overwrite = TRUE
+  ) # tk51/....git - put the repo name for work instead of "coding-demo.git"
+
+# check the active project directory
+usethis::git_remotes()
+
+gitcreds::gitcreds_get() 
+
+usethis::create_github_token()
+
+gitcreds::gitcreds_set() 
+
+credentials::set_github_pat()
+
+# palyground2
+# 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
