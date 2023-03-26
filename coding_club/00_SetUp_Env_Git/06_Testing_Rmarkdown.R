289e1696d52859f1361187660e91218bdeb0be57
tk51 <t.khamardiuk@gmail.com>
2023-03-26 16:43:48 +0200

    slight modifications

---
+++ coding_club/00_SetUp_Env_Git/06_Testing_Rmarkdown.R
@@ -0,0 +1,6 @@
+#### Testing the R Markdown ###################################################
+# check the working directory
+getwd()
+
+# setting the working directory to the one of the targeted action
+setwd("/home/t51/DataAnalytics/R-Playground")
