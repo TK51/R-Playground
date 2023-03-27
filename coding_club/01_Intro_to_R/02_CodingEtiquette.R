e9142db94f0a6412aa431d07d70526b969aa7d9f
tk51 <t.khamardiuk@gmail.com>
2023-03-27 05:52:24 +0200

    coding etiquette: content added

--- coding_club/01_Intro_to_R/02_CodingEtiquette.R
+++ coding_club/01_Intro_to_R/02_CodingEtiquette.R
@@ -1,3 +1,12 @@
+# introduction ----
+# the outline is made with four "-" added after a comment
+# it brings the structure to the document and afterwards can be seen
+# in the outline pane switched in the upper right corner ob by
+# hitting "Ctrl + Shift + O"
+
+# the beginning ----
 # get and set the working directory
 getwd()
 setwd("/home/t51/DataAnalytics/R-Playground/coding_club")
+
+# some outline ----
