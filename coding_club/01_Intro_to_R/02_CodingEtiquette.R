167c1a734d5869f2bcb4fee3229b9ccd73a5e6aa
tk51 <t.khamardiuk@gmail.com>
2023-03-27 06:14:38 +0200

    coding etiquette: content added

--- coding_club/01_Intro_to_R/02_CodingEtiquette.R
+++ coding_club/01_Intro_to_R/02_CodingEtiquette.R
@@ -1,4 +1,5 @@
-# introduction ----
+# introduction ---- 
+# the "introduction ----" can be omited for clarity
 # the outline is made with four "-" added after a comment
 # it brings the structure to the document and afterwards can be seen
 # in the outline pane switched in the upper right corner ob by
@@ -10,3 +11,36 @@ getwd()
 setwd("/home/t51/DataAnalytics/R-Playground/coding_club")
 
 # some outline ----
+
+# the basic script structure ----
+# - the introduction
+# - libraries
+# - functions
+# - working directory
+# - loaded data
+
+####################################################################
+# examples ----
+
+
+# intro ---- (the inscription itself can be omitted for clarity)
+
+# Analyzing vertebrate population change based on the 
+# Living Planet Index
+# Data available from http://www.livingplanetindex.org/
+
+# Gergana Daskalova ourcodingclub(at)gmail.com
+# 25-04-2017
+
+# Libraries ---
+library(tidyr)  # Formatting data for analysis
+library(dplyr)  # Manipulating data
+library(ggplot2)  # Visualizing results
+library(readr)  # Manipulating data
+
+# shortcuts ----
+# - Ctrl + Shift + O - outline pane
+# - Alt + L - collapse
+# - Alt + Shift + L - expand
+# - Alt + O - collapse all
+# - Alt + Shift + O - expand all
