f2e30e4742ebb03d92535b79d85dd845ca66f3d1
tk51 <t.khamardiuk@gmail.com>
2023-03-29 22:41:45 +0200

    content added

--- coding_club/01_Intro_to_R/03_BasicDataManipulations_Script.R
+++ coding_club/01_Intro_to_R/03_BasicDataManipulations_Script.R
@@ -13,6 +13,7 @@ setwd("./coding_club")
 # Load the elongation data
 elongation <- read.csv("./datasets/EmpetrumElongation.csv", header = TRUE)   
 
+#### Data exploration ----
 # Check import and preview data
 head(elongation)   # first few observations
 str(elongation)    # types of variables
@@ -31,4 +32,35 @@ elongation[6, ]
 # And of course you can mix it all together!
 elongation[6, ]$Indiv   # returns the value in the column Indiv for the sixth observation
 # (much easier calling columns by their names than figuring out where they are!)
+
+# Let's access the values for Individual number 603
+elongation[elongation$Indiv == 603, ]
+
+### Using logical operators
+# Subsetting with one condition
+
+elongation[elongation$Zone < 4, ]    # returns only the data for zones 2-3
+elongation[elongation$Zone <= 4, ]   # returns only the data for zones 2-3-4
+
+
+# This is completely equivalent to the last statement
+elongation[!elongation$Zone >= 5, ]   # the ! means exclude
+
+
+# Subsetting with two conditions
+elongation[elongation$Zone == 2 | elongation$Zone == 7, ]    # returns only data for zones 2 and 7
+elongation[elongation$Zone == 2 & elongation$Indiv %in% c(300:400), ]    # returns data for shrubs in zone 2 whose ID numbers are between 300 and 400
+
+
+
+
+
+
+
+
+
+
+
+
+
 
