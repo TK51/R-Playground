5769033650cec2593f2d12590408cf7bbfe9911a
tk51 <t.khamardiuk@gmail.com>
2023-03-29 18:10:26 +0200

    content added

---
+++ coding_club/01_Intro_to_R/03_BasicDataManipulations.R
@@ -0,0 +1,7 @@
+#### Basic Data Manipulation ----
+# 1. Learn base R syntax for data manipulation:
+# --- logical operators for finer control
+# --- creating and assigning objects
+# --- specifying factors
+# 2. Turn messy data into tidy data with tidyr
+# 3. Use efficient tools from the dplyr package to manipulate data
