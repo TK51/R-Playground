9b8292bf1c78eadf6b39f867effff2acf4db1747
tk51 <t.khamardiuk@gmail.com>
2023-03-29 19:09:52 +0200

    content added

--- coding_club/01_Intro_to_R/03_BasicDataManipulations.R
+++ coding_club/01_Intro_to_R/03_BasicDataManipulations.R
@@ -5,3 +5,9 @@
 # --- specifying factors
 # 2. Turn messy data into tidy data with tidyr
 # 3. Use efficient tools from the dplyr package to manipulate data
+
+#### 1. Subset, extract and modify data with R operators ----
+
+#### 2. What is tidy data, and how do we achieve it? ----
+
+#### 3. Explore the most common and useful functions of dplyr ----
