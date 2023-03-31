dd69c953970d9a10a2eae84732fbb3a959628b2b
tk51 <t.khamardiuk@gmail.com>
2023-03-31 08:07:47 +0200

    content added + 04_files

--- coding_club/01_Intro_to_R/04_FromDistributionsToLinearModels.R
+++ coding_club/01_Intro_to_R/04_FromDistributionsToLinearModels.R
@@ -1,18 +1,20 @@
-#### Intro ----
-# Basic Data Manipulations 
-# https://ourcodingclub.github.io/tutorials/modelling/index.html
-# 2023_03_31
-# Taras Khamardiuk (https://www.kaggle.com/taraskhamardiuk)
-
-#### Working directory ----
-# Set working directory
-getwd()
-setwd("./coding_club")
-
-#### Data source ----
-# Load the elongation data
+#### FROM DISTRIBUTIONS TO LINEAR MODELS ----
+# GETTING COMFORTABLE WITH THE BASICS OF STATISTICS MODELLING
 
 
+# 1. Learn base R syntax for data manipulation:
+# --- logical operators for finer control
+# --- creating and assigning objects
+# --- specifying factors
+# 2. Turn messy data into tidy data with tidyr
+# 3. Use efficient tools from the dplyr package to manipulate data
 
-#### Data exploration ----
-# Check import and preview data
+# Tutorial Aims & Steps:
+# 1. Get familiar with different data distributions
+# 2. Choosing your model structure
+# 3. Practice linear models (and ANOVAs)
+# --- Write and run the models
+# --- Understand the outputs
+# --- Verify the assumptions
+# 4. Practice generalized linear models
+# 5. Challenge yourself!
---
+++ coding_club/01_Intro_to_R/04_FromDistributionsToLinearModels_Script.R
@@ -0,0 +1,21 @@
+#### Intro ----
+# Basic Data Manipulations 
+# https://ourcodingclub.github.io/tutorials/modelling/index.html
+# 2023_03_31
+# Taras Khamardiuk (https://www.kaggle.com/taraskhamardiuk)
+
+#### Libraries ----
+
+
+#### Working directory ----
+# Set working directory
+getwd()
+setwd("./coding_club")
+
+#### Data source ----
+# Load the elongation data
+
+
+
+#### Data exploration ----
+# Check import and preview data
