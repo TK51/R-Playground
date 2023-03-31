e768b1c46f4e34211d730c7a2363623f88fc8579
tk51 <t.khamardiuk@gmail.com>
2023-03-31 07:18:17 +0200

    content added

--- coding_club/01_Intro_to_R/03_Challenge_Data_Dragons.R
+++ coding_club/01_Intro_to_R/03_Challenge_Data_Dragons.R
@@ -56,6 +56,15 @@ names(drags)                  # returns the names of the columns
 
 names(drags)[6] <- "turmeric" # change the name from "paprika" to "turmeric"
 
+# Fix the calibration error for tabasco by horntail
+
+# create a vector of corrected values
+correct.values  <- drags$tabasco[drags$species == 'hungarian_horntail'] - 30  
+
+# overwrite the values in the dragons object
+drags[drags$species == 'hungarian_horntail', 'tabasco'] <- correct.values      
+head(drags)
+
 ### CREATING A FACTOR
 
 # Let's check the classes
@@ -75,26 +84,93 @@ levels(drags$species)  # shows the different factor levels
 
 #### TIDY DATA ----
 library(tidyr)              # load the package
+library(dplyr)
 
 drags_long <- gather(drags, spice, length,                           # in this order: data frame, key, value
                           c("jalapeno", "turmeric", "tabasco", "wasabi"))        # we need to specify which columns to gather
+
+#### SOLUTION - alternative
+# Reshape the data from wide to long format
+
+dragons_long <- gather(dragons, key = 'spice', value = 'plume', 
+                       c('tabasco', 'jalapeno', 'wasabi', 'turmeric'))
+
+
 head(drags_long)
 
 boxplot(length ~ species, data = drags_long,
         xlab = "Year", ylab = "Elongation (cm)",
         main = "Annual growth of Empetrum hermaphroditum")
 
+# Create new column with length in meters
+drags_long <- mutate(drags_long, plume.m = length / 100)
+head(drags_long)  # getting the length in meters
+
+
+
+
+
 # --- progress here ----
 
 
+#### THE SOLUTION
+## Load data
 
-# for the tabasco 
-# trial, but only for the Hungarian Horntail species. All measurements are 30 cm 
-# higher than they should be.
+dragons <- read.csv('dragons.csv', header = TRUE)
 
-# 3. The lengths are given in centimeters, but really it would make sense to 
-# convert them to meters.
 
+## Clean the dataset
+
+# Change paprika to turmeric
+
+dragons <- rename(dragons, turmeric = paprika)
+
+
+# Fix the calibration error for tabasco by horntail
+
+correct.values  <- dragons$tabasco[dragons$species == 'hungarian_horntail'] - 30   # create a vector of corrected values
+
+dragons[dragons$species == 'hungarian_horntail', 'tabasco'] <- correct.values      # overwrite the values in the dragons object
+
+# to change these values after reshaping the data to long format [dplyr].
+dragons.2 <- mutate(dragons, tabasco = ifelse(species == 'hungarian_horntail', 
+                                              tabasco - 30, tabasco))
+
+# This creates (overwrites) the column tabasco using the following logic: if the species is Hungarian Horntail, deduct 30 from the values in the (original) tabasco column; if the species is NOT horntail (i.e. all other species), write the original values.
+# Reshape the data from wide to long format
+
+dragons_long <- gather(dragons, key = 'spice', value = 'plume', c('tabasco', 'jalapeno', 'wasabi', 'turmeric'))
+
+
+# Convert the data into meters
+
+dragons_long <- mutate(dragons_long, plume.m = plume/100)    # Creating a new column turning cm into m
+
+
+# Create a subset for each species to make boxplots
+
+horntail <- filter(dragons_long, species == 'hungarian_horntail')            # the dplyr way of filtering
+green <- filter(dragons_long, species == 'welsh_green')
+shortsnout <- dragons_long[dragons_long$species == 'swedish_shortsnout', ]   # maybe you opted for a base R solution instead?
+
+
+# Make the boxplots
+
+par(mfrow=c(1, 3))      # you need not have used this, but it splits your plotting device into 3 columns where the plots will appear, so all the plots will be side by side.
+
+boxplot(plume.m ~ spice, data = horntail,
+        xlab = 'Spice', ylab = 'Length of fire plume (m)',
+        main = 'Hungarian Horntail')
+
+
+boxplot(plume.m ~ spice, data = green,
+        xlab = 'Spice', ylab = 'Length of fire plume (m)',
+        main = 'Welsh Green')
+
+
+boxplot(plume.m ~ spice, data = shortsnout,
+        xlab = 'Spice', ylab = 'Length of fire plume (m)',
+        main = 'Swedish Shortsnout')
 
 
 
