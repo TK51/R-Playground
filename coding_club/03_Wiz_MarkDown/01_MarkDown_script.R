bc10e40db5bd507b2c7e67c6f4404dc5f68fa05c
tk51 <t.khamardiuk@gmail.com>
2023-04-08 07:46:56 +0200

    mddwn created

---
+++ coding_club/03_Wiz_MarkDown/01_MarkDown_script.R
@@ -0,0 +1,31 @@
+#### EGETTING STARTED WITH R MARKDOWN - Script
+
+#### Intro ----
+# CREATING A NEAT AND REPRODUCIBLE RECORD OF YOUR CODE
+# https://ourcodingclub.github.io/tutorials/rmarkdown/index.html
+# 2023_04_08
+# Taras Khamardiuk (https://www.kaggle.com/taraskhamardiuk)
+
+#### Description ----
+# Tutorial AIMS:
+# 1. Understand what RMarkdown is and why you should use it
+# 2. Learn how to construct an RMarkdown file
+# 3. Export an RMarkdown file into many file formats
+# Tutorial STEPS
+# 1. What is RMarkdown
+# 2. Download RMarkdown
+# 3. Create an RMarkdown (.Rmd) file
+# 4. YAML header material
+# 5. Markdown syntax
+# 6. Insert code from an R script into a .Rmd file
+# 7. Create a .pdf file from your .Rmd file
+# 8. R Notebooks (the future of reproducible code? Maybe?)
+
+# clean the slate and start from the clear page
+rm(list = ls())
+
+#### Libraries ----
+
+ 
+
+
