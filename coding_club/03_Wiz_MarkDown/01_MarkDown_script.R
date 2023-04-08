d8e94bd1cba7292dca019021e9c217135f1b856e
tk51 <t.khamardiuk@gmail.com>
2023-04-08 08:10:08 +0200

    update

--- coding_club/03_Wiz_MarkDown/01_MarkDown_script.R
+++ coding_club/03_Wiz_MarkDown/01_MarkDown_script.R
@@ -31,7 +31,64 @@ library(rmarkdown)
 #### Working directory ----
 # Set working directory
 getwd()
-setwd("./coding_club")
+setwd("./03_Wiz_MarkDown")
+
+#### 1. What is RMarkdown ----
+# Markdown is a very simple ‘markup’ language which provides methods for 
+# creating documents with headers, images, links etc. from plain text files, 
+# while keeping the original plain text file easy to read. You can convert 
+# Markdown documents to many other file types like .html or .pdf to display 
+# the headers, images etc.
+
+# 2. Download RMarkdown ----
+#install.packages("rmarkdown")          # - if you're missing one
+
+# 3. Create an RMarkdown (.Rmd) file ----
+# To create a new RMarkdown file (.Rmd), select File -> New File -> R Markdown..._ 
+# in RStudio, then choose the file type you want to create. For now we will 
+# focus on a .html Document, which can be easily converted to other file types later.
+
+# The newly created .Rmd file comes with basic instructions, but we want to 
+# create our own RMarkdown script, so go ahead and delete everything in the 
+# example file.
+
+# Now open the RMarkdown_Tutorial.R practice script from the repository you 
+# downloaded earlier in another tab in RStudio and use the instructions below to 
+# help you convert this script into a coherent RMarkdown document, bit by bit.
+
+
+
+# 4. YAML header material
+# 5. Markdown syntax
+# 6. Insert code from an R script into a .Rmd file
+# 7. Create a .pdf file from your .Rmd file
+# 8. R Notebooks (the future of reproducible code? Maybe?)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
  
 
 
