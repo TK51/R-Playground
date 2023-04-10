698d4c7c33be8e024b65e2b16e6c92dd6efa0abb
tk51 <t.khamardiuk@gmail.com>
2023-04-10 08:00:53 +0200

    shiny content update

--- coding_club/04_ShinyR/tk_app/app.R
+++ coding_club/04_ShinyR/tk_app/app.R
@@ -1,7 +1,7 @@
 # Packages ----
-library(shiny)  # Required to run any Shiny app
+library(shiny)    # Required to run any Shiny app
 library(ggplot2)  # For creating pretty plots
-library(dplyr)  # For filtering and manipulating data
+library(dplyr)    # For filtering and manipulating data
 library(agridat)  # The package where the data comes from
 
 # Loading data ----
