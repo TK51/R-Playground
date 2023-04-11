e6472d4b09411c12637c40eacff431b38f46e70d
tk51 <t.khamardiuk@gmail.com>
2023-04-11 22:54:34 +0200

    books update

--- books/hands_on_programming_R/01_the_very_basics.R
+++ books/hands_on_programming_R/01_the_very_basics.R
@@ -47,6 +47,37 @@ die %*% die
 # matrix outer multiplication (with %o%)
 die %o% die
 
+# basic functins - round and factorial
+round(3.1567)  # 3
+factorial(3)   # 6
+
+# function logic - from innermost to the outermost
+mean(1:6)  # 3.5
+
+mean(die)  # 3.5
+
+round(mean(die))  # 4
+
+# you can simulate a roll of the die with R’s sample function, takes 2 args -
+# a vector named x and a number named size.
+sample(x = 1:6, size = 2)  # 2 5 or 1 3 or 2 3
+
+# roll a die by setting x = die and size = 1 (as one element to be rolled)
+sample(x = die, size = 1) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
