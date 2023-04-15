5789e7b9b5ca6ff95e06f5db339c7b543e0ad7a4
tk51 <t.khamardiuk@gmail.com>
2023-04-15 18:09:00 +0200

    ...

--- books/hands_on_programming_R/04_R_notation.R
+++ books/hands_on_programming_R/04_R_notation.R
@@ -3,10 +3,28 @@
 # https://rstudio-education.github.io/hopr/r-notation.html
 # 2023_04_13
 # Taras Khamardiuk (https://www.kaggle.com/taraskhamardiuk)
-#
 
 #### R Notation ----
 
 # Working directory
 getwd()
 setwd("/home/t51/DataAnalytics/R-Playground/books/hands_on_programming_R")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
