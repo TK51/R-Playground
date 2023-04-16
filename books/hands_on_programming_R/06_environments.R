9f68dbbf4f587934162ef1bef9f9e372e41260e0
tk51 <t.khamardiuk@gmail.com>
2023-04-16 15:59:46 +0200

    envs

--- books/hands_on_programming_R/06_environments.R
+++ books/hands_on_programming_R/06_environments.R
@@ -1,7 +1,7 @@
 #### Hands-On Programming with R by Garrett Grolemund - Script
 # 8 Environments
 # https://rstudio-education.github.io/hopr/environments.html
-# 2023_04_15
+# 2023_04_16
 # Taras Khamardiuk (https://www.kaggle.com/taraskhamardiuk)
 
 #### Environments ----
@@ -13,3 +13,28 @@ setwd("/home/t51/DataAnalytics/R-Playground/books/hands_on_programming_R")
 # Data used (as "deck")
 deck <- read.csv("datasets/cards.csv")
 head(deck)
+
+# Your deck is now ready for a game of blackjack (or hearts or war), but are your 
+# shuffle and deal functions up to snuff? Definitely not. For example, deal deals 
+# the same card over and over again
+
+# And the shuffle function doesn’t actually shuffle deck (it returns a copy of deck
+# that has been shuffled). In short, both of these functions use deck, but neither 
+# manipulates deck—and we would like them to.
+
+# To fix these functions, you will need to learn how R stores, looks up, and 
+# manipulates objects like deck. R does all of these things with the help of an 
+# environment system.
+
+
+
+
+
+
+
+
+
+
+
+
+
