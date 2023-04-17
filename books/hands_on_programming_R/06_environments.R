479aad24051e08adaa7abafdd88e0d96c560dbe3
tk51 <t.khamardiuk@gmail.com>
2023-04-17 20:28:26 +0200

    envs

--- books/hands_on_programming_R/06_environments.R
+++ books/hands_on_programming_R/06_environments.R
@@ -111,7 +111,20 @@ globalenv()$new
 
 #### 8.2.1 The Active Environment ----
 
+# use environment to see the current active environment:
+environment()
+## <environment: R_GlobalEnv>
 
+#### 8.3 Scoping Rules ----
+# R follows a special set of rules to look up objects. These rules are known as 
+# R’s scoping rules, and you’ve already met a couple of them:
+# 1. R looks for objects in the current active environment.
+# 2. When you work at the command line, the active environment is the global 
+# environment. Hence, R looks up objects that you call at the command line in 
+# the global environment.
+# Here is a third rule that explains how R finds objects that are not in the 
+# active environment
+# 3 .When R does not find an object in an environment, R looks in the environment’s parent environment, then the parent of the parent, and so on, until R finds the object or reaches the empty environment.
 
 
 
